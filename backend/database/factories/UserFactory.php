12fb72540b8e2d9af139aafb5efcedc7924e962d
DeliaGlez <deliavargas.glez@gmail.com>
2025-05-26 15:45:30 -0700

    Arreglo seeder usuarios telefono

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -29,7 +29,7 @@ class UserFactory extends Factory
             //'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
-            'phone'=> fake()->phoneNumber(),
+            'phone' => $this->faker->numerify('##########'),
             'birthdate' => fake()->date(),
             'address'=>fake()->address(),
             'file_path'=>'profile_pictures/default.jpg',
